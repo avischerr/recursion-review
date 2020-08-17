7d838510d6b5446714a22c8d5ba72fc9a00d144d
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-08-17 15:07:45 -0500

    Pass all tests for stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,14 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// You should use document.body, element.childNodes, and element.classList
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // create results array
+  var results = [];
+  // check if node's class is equal to target class
+  // check node's children
+  // return results
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,30 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // stringify a number, null, true and false
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    var resultArray = [];
+    for (var i = 0; i < obj.length; i++) {
+      var current = stringifyJSON(obj[i]);
+      resultArray.push(current);
+    }
+    resultArray.join(',');
+    return '[' + resultArray + ']';
+  } else {
+    var result = '';
+    for (var key in obj) {
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    result = result.slice(0, result.length - 1);
+    return '{' + result + '}';
+  }
 };
