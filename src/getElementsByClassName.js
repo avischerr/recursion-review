adc476eb9accfa214e3de541d501550f6647b028
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-08-17 16:29:25 -0500

    begin work on getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,11 +6,27 @@
 // You should use document.body, element.childNodes, and element.classList
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
+var getElementsByClassName = function(className, currentNode) {
   // create results array
   var results = [];
   // check if node's class is equal to target class
-  // check node's children
+  // target element type? elements[i].nodeType === 1
+  if (currentNode) {
+    if (currentNode.classList.contains(className)) {
+      results.push(currentNode);
+    }
+
+    if (currentNode.hasChildNodes()) {
+      for (var i = 0; i < currentNode.childNodes.length; i++) {
+        getElementsByClassName(className, currentNode.childNodes[i]);
+      }
+    }
+  }
+
+  getElementsByClassName(className, document.body);
+
   // return results
+  console.log(results);
   return results;
+
 };
